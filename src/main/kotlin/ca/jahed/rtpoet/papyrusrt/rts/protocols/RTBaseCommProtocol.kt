77406ca8f88a30277ba4c971de941a81692e8d7f
Karim Jahed <jahed@cs.queensu.ca>
2021-04-22 09:52:01 -0400

    Moved rtBound & rtUnbound to main RTPoet

--- src/main/kotlin/ca/jahed/rtpoet/papyrusrt/rts/protocols/RTBaseCommProtocol.kt
+++ src/main/kotlin/ca/jahed/rtpoet/papyrusrt/rts/protocols/RTBaseCommProtocol.kt
@@ -1,22 +1,5 @@
 package ca.jahed.rtpoet.papyrusrt.rts.protocols
 
-import ca.jahed.rtpoet.rtmodel.rts.RTSystemSignal
 import ca.jahed.rtpoet.rtmodel.rts.protocols.RTSystemProtocol
 
-object RTBaseCommProtocol : RTSystemProtocol("UMLRTBaseCommProtocol") {
-    private object RTBound : RTSystemSignal("rtBound")
-    private object RTUnbound : RTSystemSignal("rtUnbound")
-
-    init {
-        inOutSignals.add(RTBound)
-        inOutSignals.add(RTUnbound)
-    }
-
-    fun rtBound(): RTSystemSignal {
-        return RTBound
-    }
-
-    fun rtUnbound(): RTSystemSignal {
-        return RTUnbound
-    }
-}
+object RTBaseCommProtocol : RTSystemProtocol("UMLRTBaseCommProtocol")
